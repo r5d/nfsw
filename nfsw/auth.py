879a10131d8cc5250484bc02d29941ff571f6854
rsiddharth <s@ricketyspace.net>
2019-09-13 22:11:44 -0400

    nfsw/auth.py: Add login route.

--- nfsw/auth.py
+++ nfsw/auth.py
@@ -49,3 +49,39 @@ def not_agreed(view):
     return wrapped_view
 
 
+@bp.route('/login', methods=('GET', 'POST'))
+@anon_only
+def login():
+    def render(e=''):
+        if e:
+            flash(e)
+
+        return render_template('login.html')
+
+    db = get_db()
+
+    if request.method == 'POST':
+
+        username = request.form['username']
+        password = request.form['password']
+
+        # Validate
+        if not username:
+            return render('Name is required')
+        elif not password:
+            return render('Password is required')
+
+        user  = db.execute('SELECT * FROM user WHERE username=?',
+                           (username,)).fetchone()
+
+        if user is None:
+            return render('User not found')
+        elif not check_password_hash(user['password'], password):
+            return render('Password is incorrect')
+
+        session.clear()
+        session['user_id'] = user['id']
+
+    return render()
+
+

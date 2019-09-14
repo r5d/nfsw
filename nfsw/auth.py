879b5e3d624b64fa6fbe1d5a4bbe125ad568f567
rsiddharth <s@ricketyspace.net>
2019-09-13 22:16:19 -0400

    nfsw/auth.py: Add load_logged_in_user
    It's a bp.before_app_request function.

--- nfsw/auth.py
+++ nfsw/auth.py
@@ -165,3 +165,17 @@ def logout():
     return redirect(url_for('index'))
 
 
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id=?', (user_id,)
+        ).fetchone()
+
+        if (request.endpoint not in ['auth.terms', 'auth.sorry']
+            and g.user['terms_agreed'] != 1):
+            return redirect(url_for('auth.terms'))

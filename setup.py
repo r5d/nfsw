d1212e64df36ac3b917d44526a216b4f0ce28950
rsiddharth <s@ricketyspace.net>
2019-12-25 12:58:43 -0500

    Bump to version 0.1.9.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='nfsw',
-    version='0.1.8',
+    version='0.1.9',
     license='ISC',
     author='rsiddharth',
     author_email='s@ricketyspace.net',

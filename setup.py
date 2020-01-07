5c6d8dca25503ccf8511496ef13ffb1baab866b3
rsiddharth <s@ricketyspace.net>
2020-01-07 16:21:09 -0500

    Bump to 0.2.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='nfsw',
-    version='0.2.0',
+    version='0.2.1',
     license='ISC',
     author='rsiddharth',
     author_email='s@ricketyspace.net',

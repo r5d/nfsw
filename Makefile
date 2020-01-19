e124670447c9294a5dba5654b94574da8698cb4f
rsiddharth <s@ricketyspace.net>
2020-01-18 20:27:29 -0500

    Bump to version 0.2.2.

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@
 #   This file is part of nfsw.
 #
 
-VERSION=0.2.1
+VERSION=0.2.2
 
 VENV_DIR=/usr/local/virtualenv/.nfsw
 VENV_CMD=virtualenv-3
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='nfsw',
-    version='0.2.1',
+    version='0.2.2',
     license='ISC',
     author='rsiddharth',
     author_email='s@ricketyspace.net',
